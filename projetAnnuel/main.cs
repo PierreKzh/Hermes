bc3341211c14868192eee7d84cb038d6c153ba38
Pierre KERZERHO <kerzerho.p@lyceedelasalle.fr>
2021-12-06 20:06:22 +0100

    Ajoutez des fichiers projet.

---
+++ projetAnnuel/main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using listener;
+//using sender;
+class main
+{
+    public static int Main(String[] args)
+    {
+        /*sender.client client1 = new sender.client();
+        server server1 = new server();
+        server.StartServer();
+        //client.StartClient();*/
+        return 0;
+    }
+}
