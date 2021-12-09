1948ded2ee4172b854cb53a08520d95d7726c7ec
Pierre Kerzerho <kerzerho.pierre13@gmail.com>
2021-12-09 23:36:29 +0100

    refonte branch

---
+++ Hermes/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using listener;
+using sender;
+class main
+{
+    /**
+     * @file main.cs
+     *
+     * @brief principal program
+     * @details 
+     * @author Pierre Kerzerho <kerzerho.pierre13@gmail.com>
+     * @author 
+     * @version 0.1
+     *
+     * @return int
+     *
+     */
+    public static int Main(String[] args)
+    {
+        client client1 = new client();
+        server server1 = new server();
+        server.StartServer("127.0.0.1", 11000);
+        client.StartClient("127.0.0.1", 11000, "test message");
+        return 0;
+    }
+}
