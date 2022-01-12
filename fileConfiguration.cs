718aa5330ba28994ca65cba19ef83eb82d6e78fd
Pierre Kerzerho <kerzerho.pierre13@gmail.com>
2022-01-12 20:01:39 +0100

    correction bug, that canceled the creation of the file

--- fileConfiguration.cs
+++ fileConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using contactStruct;
 
 namespace fileConfiguration
@@ -18,6 +19,13 @@ namespace fileConfiguration
      */
         public DataFile()
         {
+            if (!File.Exists(file)) 
+            {
+                Stream stream = File.Open(DataFile.file, FileMode.Create);
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, MemberwiseClone());
+                stream.Close();
+            }
         }
         private static string file = "HermesData.data";
         public List<Contact> contacts = new List<Contact>();
@@ -26,9 +34,9 @@ namespace fileConfiguration
             /**
              * @brief   write the content of DataFIle in a file
              */
-            System.IO.Stream stream = System.IO.File.Open(DataFile.file, System.IO.FileMode.OpenOrCreate);
+            Stream stream = File.Open(DataFile.file, FileMode.Open);
             var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-            binaryFormatter.Serialize(stream, this.MemberwiseClone());
+            binaryFormatter.Serialize(stream, MemberwiseClone());
             stream.Close();
         }
         public DataFile ReadFile()
@@ -37,7 +45,7 @@ namespace fileConfiguration
              * @brief   read the content of a file to return it in DataFile
              * @return  DataFile :  The DataFile read in a file
              */
-            System.IO.Stream stream = System.IO.File.Open(DataFile.file, System.IO.FileMode.Open);
+            Stream stream = File.Open(DataFile.file, FileMode.Open);
             var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
             DataFile dataFile = (DataFile)binaryFormatter.Deserialize(stream);
             stream.Close();
