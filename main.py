0c8b36ba310adc3cd614cb8ae5de52c54912f061
Pierre Kerzerho <kerzerho.pierre13@gmail.com>
2022-03-03 14:10:01 +0100

    create user interface with encrypted file and users creation

--- main.py
+++ main.py
@@ -1,94 +1,19 @@
-import addContactUI
+import connexionUI
 import sys
-import json
-from os import path
-import re
 
-#variables
-file = "HermesData.json"
+import tools
 
 
-#class
-class Contact:
-    def __init__(self, Username, IPaddress):
-        self.Username = Username
-        self.IPaddress = IPaddress
-
-    def toJson(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
-
-class Payload(object):
-    def __init__(self, j):
-        self.__dict__ = json.loads(j)
-
-
-#functions
 def hermesUI_start():
-    app = addContactUI.QtWidgets.QApplication(sys.argv)
-    MainWindow = addContactUI.QtWidgets.QMainWindow()
-    ui = addContactUI.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    """
+    Create and lunch the first window
+    """
+    app = connexionUI.QtWidgets.QApplication(sys.argv)
+    MainWindow = connexionUI.QtWidgets.QMainWindow()
+    uiMainWindow = connexionUI.Ui_connexion()
+    uiMainWindow.setupUi(MainWindow)
     sys.exit(app.exec_())
-
-def WriteFile(listContact):
-        with open(file, 'w') as json_file:
-            json.dump(listContact, json_file)
-
-def ReadFile():
-    with open(file) as fp:
-        listContact = json.load(fp)
-    return listContact
-
-def addContact(self):
-    listContact = ReadFile()
-    ValidIpAddressRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
-    self.Username = str(self.lineEdit_Username.text())
-    self.IPaddress = str(self.lineEdit_IPaddress.text())
-    if (self.Username != '' and self.IPaddress != ''):
-        if(re.match(ValidIpAddressRegex,self.IPaddress)):
-            listContact.append(Contact(self.Username, self.IPaddress).toJson())
-            WriteFile(listContact)
-            self.lineEdit_Username.setText('')
-            self.lineEdit_IPaddress.setText('')
-            displayListWidget_contacts(self)
-            self.label_IPaddressEvent.setText('')
-        else:
-            self.label_IPaddressEvent.setStyleSheet("color: red")
-            self.label_IPaddressEvent.setText("Invalid IP address")
-
-def deleteContact(self):
-    listContact = ReadFile()
-    newListContact = []
-    row = self.listWidget_contacts.currentRow()
-    i = len(listContact)-1
-    for contact in listContact:
-        if i != row:
-            newListContact.append(contact)
-        else:
-            print("on ajoute pas"+contact)
-        i-=1
-    WriteFile(newListContact)
-    displayListWidget_contacts(self)
-
-def displayListWidget_contacts(self):
-    if path.isfile(file) is False:
-        listContact = []
-        with open(file, 'w') as json_file:
-            json.dump(listContact, json_file)
-    listContact = ReadFile()
-    self.listWidget_contacts.clear()
-    for contact in listContact:
-        contact = Payload(contact)
-        self.listWidget_contacts.insertItem(0, contact.Username)
-
-
-#code
 
 
 if __name__ == '__main__':
     hermesUI_start()
-    """list = []
-    list.append(Contact(Username="pierre", IPaddress="127.0.0.1").toJson())
-    list.append(Contact(Username="JB", IPaddress="127.0.0.2").toJson())
-    WriteFile(list)"""
---
+++ tools.py
@@ -0,0 +1,96 @@
+import binascii
+import json
+import re
+import hashlib
+import pyaes
+from os import path
+
+
+#variables
+file = "HermesData.json"
+salt = "iU9{nWRx8*B5^4:U!d8k"
+saltEncrypt = "x3HMcVP>76fk7c$>-uc-86U6p~,N3D"
+
+#functions
+def WriteFile(currentPassword, data, item):
+    """
+    Open the data file, decrypt it and write in datas selected
+    :param currentPassword: user password
+    :param data: data to write
+    :param item: wich data write in the file
+    :return: binary in str
+    """
+    #write all the file
+    if item == "datas":
+        with open(file, 'w') as json_file:
+            json.dump(data, json_file)
+    #write a part of the file
+    else:
+        fullDatas = ReadFile(currentPassword, "datas")
+        #write contacts
+        if item == "contacts":
+            datas = fullDatas['datas']
+            datas['contacts'] = data
+        #encryption of the file
+        datasEncrypt = toAES(str(datas), toPasswordEncode(currentPassword))
+        fullDatas['datas'] = datasEncrypt
+        with open(file, 'w') as json_file:
+            json.dump(fullDatas, json_file)
+
+def ReadFile(currentPassword, item):
+    """
+    Get datas in the file
+    :param currentPassword: user password
+    :param item: wich data get
+    :return: json data
+    """
+    with open(file) as fp:
+        dataFile = json.load(fp)
+    #return the hash of the file
+    if item == "hash":
+        return dataFile['hash']
+    elif (dataFile['datas']):
+        password = toPasswordEncode(currentPassword).encode('utf-8')
+        aes = pyaes.AESModeOfOperationCTR(password)
+        #decrypted data is always binary, need to decode to plaintext
+        datasDecrypt = aes.decrypt(bytes.fromhex(dataFile['datas'])).decode('utf-8')
+        datasJson = json.loads(datasDecrypt.replace("\'", "\""))
+        #return all datas
+        if item == "datas":
+            fullData = {"hash": dataFile['hash'], 'datas': datasJson}
+            return fullData
+        #return contacts
+        if item == "contacts":
+            return datasJson['contacts']
+
+def toAES(plainText, password):
+    """
+    Encrypt data into AES
+    :param plainText: original text
+    :param password: password to encrypt text
+    :return: Encrypted text in hexa
+    """
+    key = str(password).encode('utf-8')
+    #password must be byte
+    aes = pyaes.AESModeOfOperationCTR(key)
+    #encrypt test in AES to hexa
+    cipherText = aes.encrypt(plainText)
+    return cipherText.hex()
+
+def toSHA(password):
+    """
+    transform text into SHA
+    :param password: original text
+    :return: SHA in hexa
+    """
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def toPasswordEncode(password):
+    """
+    transform password into 16 bytes key to do AES256
+    :param password: original password
+    :return: 16 bytes key
+    """
+    key = toSHA(saltEncrypt+password)[:16]
+    return key
+
