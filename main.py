7f4a94b363105a75ecb76a9fe3ccadd9ce85cd1c
JeanPiffre <jb21.fave@gmail.com>
2022-04-08 09:41:56 +0200

    Page de connexion avec SQLite

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import connexionUI
 import sys
-
-import tools
-
+from PyQt5.QtWidgets import QApplication, QWidget
+from connexionUI import *
 
 def hermesUI_start():
     """
     Create and lunch the first window
     """
-    app = connexionUI.QtWidgets.QApplication(sys.argv)
-    MainWindow = connexionUI.QtWidgets.QMainWindow()
-    uiMainWindow = connexionUI.Ui_connexion()
-    uiMainWindow.setupUi(MainWindow)
+    app = QtWidgets.QApplication(sys.argv)
+    connexion = QtWidgets.QMainWindow()
+    uiConnect = Ui_connexion()
+    uiConnect.setupUi(connexion)
+    connexion.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     hermesUI_start()
