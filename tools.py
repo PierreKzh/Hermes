c092d86b4ea3479ce699eed7f2037a978a64fac0
JeanPiffre <jb21.fave@gmail.com>
2022-04-20 13:10:15 +0200

    Ajout du chiffrement des données dans la bdd (users)

--- tools.py
+++ tools.py
@@ -1,95 +1,67 @@
-import binascii
-import json
-import re
-import hashlib
-import pyaes
-from os import path
+import os, binascii, codecs
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto import Random
+from binascii import b2a_hex
+import sys
+
+class crypto(object):
+    
+    def encrypted(self, password, data):
+        try:
+            print("===CHIFFREMENT EN COURS===")
+            iv = Random.new().read(AES.block_size)                          #IV
+            salt = binascii.unhexlify('7d34f60be198c7397de94885a7489390')   # Salt
+            key = binascii.hexlify(PBKDF2(password, salt, dkLen=16))        # Key
+        
+            # Use key and iv to initialize AES object
+            mycipher = AES.new(key, AES.MODE_CFB, iv)
+        
+            # Add iv to the beginning of the encrypted ciphertext
+            ciphertext = iv + mycipher.encrypt(data.encode())
+            ciphertext = ciphertext.hex()
+        
+            return ciphertext
+        except:
+            print("===CHIFFREMENT ERREUR===")
+            return None
 
 
-#variables
-file = "HermesData.json"
-salt = "iU9{nWRx8*B5^4:U!d8k"
-saltEncrypt = "x3HMcVP>76fk7c$>-uc-86U6p~,N3D"
+    def decrypted(self, password, data):
+        try:
+            print("===DECHIFFREMENT EN COURS===")
+            salt = binascii.unhexlify('7d34f60be198c7397de94885a7489390')   # Salt
+            key = binascii.hexlify(PBKDF2(password, salt, dkLen=16))        # Key
+            data = binascii.unhexlify(data)                                 
+        
+            # Use key and iv to generate a new AES object
+            mydecrypt = AES.new(key, AES.MODE_CFB, data[:16])
 
-#functions
-def WriteFile(currentPassword, data, item):
-    """
-    Open the data file, decrypt it and write in datas selected
-    :param currentPassword: user password
-    :param data: data to write
-    :param item: wich data write in the file
-    :return: binary in str
-    """
-    #write all the file
-    if item == "datas":
-        with open(file, 'w') as json_file:
-            json.dump(data, json_file)
-    #write a part of the file
+            # Decrypt the encrypted ciphertext
+            decrypttext = mydecrypt.decrypt(data[16:])
+            decrypttext = decrypttext.decode()
+        
+            return decrypttext
+        except:
+            print("===DECHIFFREMENT ERREUR===")
+            return None
+
+
+if __name__ == '__main__':
+    password = 'password'
+    text = 'message'
+
+    crypto = crypto()
+    encrypttext = crypto.encrypted(password, text)
+    print("The encrypted data is: ",encrypttext)
+    
+    decrypttext = crypto.decrypted(password, encrypttext)
+    print("The decrypted data is: ", decrypttext)
+    
+    print("Message : ", text)
+    
+    if text == decrypttext:
+        print("OK")
+        
     else:
-        fullDatas = ReadFile(currentPassword, "datas")
-        #write contacts
-        if item == "contacts":
-            datas = fullDatas['datas']
-            datas['contacts'] = data
-        #encryption of the file
-        datasEncrypt = toAES(str(datas), toPasswordEncode(currentPassword))
-        fullDatas['datas'] = datasEncrypt
-        with open(file, 'w') as json_file:
-            json.dump(fullDatas, json_file)
-
-def ReadFile(currentPassword, item):
-    """
-    Get datas in the file
-    :param currentPassword: user password
-    :param item: wich data get
-    :return: json data
-    """
-    with open(file) as fp:
-        dataFile = json.load(fp)
-    #return the hash of the file
-    if item == "hash":
-        return dataFile['hash']
-    elif (dataFile['datas']):
-        password = toPasswordEncode(currentPassword).encode('utf-8')
-        aes = pyaes.AESModeOfOperationCTR(password)
-        #decrypted data is always binary, need to decode to plaintext
-        datasDecrypt = aes.decrypt(bytes.fromhex(dataFile['datas'])).decode('utf-8')
-        datasJson = json.loads(datasDecrypt.replace("\'", "\""))
-        #return all datas
-        if item == "datas":
-            fullData = {"hash": dataFile['hash'], 'datas': datasJson}
-            return fullData
-        #return contacts
-        if item == "contacts":
-            return datasJson['contacts']
-
-def toAES(plainText, password):
-    """
-    Encrypt data into AES
-    :param plainText: original text
-    :param password: password to encrypt text
-    :return: Encrypted text in hexa
-    """
-    key = str(password).encode('utf-8')
-    #password must be byte
-    aes = pyaes.AESModeOfOperationCTR(key)
-    #encrypt test in AES to hexa
-    cipherText = aes.encrypt(plainText)
-    return cipherText.hex()
-
-def toSHA(password):
-    """
-    transform text into SHA
-    :param password: original text
-    :return: SHA in hexa
-    """
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def toPasswordEncode(password):
-    """
-    transform password into 16 bytes key to do AES256
-    :param password: original password
-    :return: 16 bytes key
-    """
-    key = toSHA(saltEncrypt+password)[:16]
-    return key
+        print("NOT")
